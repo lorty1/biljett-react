e2231971a9a51420f4a48652149a674c27a8832a
will <r>
2019-11-20 18:46:09 +0100

    knacss package scss create

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -4,14 +4,21 @@ import { hot } from "react-hot-loader";
 
 const App = () => {
   console.log('ttt')
-axios({
-  method: 'get',
-  url: 'api/departure/'
-}).then(response => {
-  console.log('pp')
-  console.log(response.data)
-})
-  return <div className={style.app}>react Starter 🚀 </div>;
+  axios({
+    method: 'get',
+    url: 'api/departure/'
+  }).then(response => {
+    console.log('pp')
+    console.log(response.data)
+  })
+  return (
+    <div>
+      <div className="flex-container">
+        <div className={style.app}>react Starter 🚀 </div>;
+        <button className="btn--success">Valider</button>
+      </div>
+    </div>
+  )
 };
 
 export default hot(module)(App);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
+import './assets/sass/knacss.scss'
 
 console.log('enter')
 ReactDOM.render(<App />, document.getElementById("root"));
